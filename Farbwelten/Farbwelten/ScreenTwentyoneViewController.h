87d78c9f5b6b8d356a13561efe0699f5d60088e4
Ida Buchwald <ida91@web.de>
2013-06-29 18:38:09 +0200

    Framework for part three finished so far

---
+++ Farbwelten/Farbwelten/ScreenTwentyoneViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScreenTwentyoneViewController.h
+//  Farbwelten
+//
+//  Created by Ida Buchwald on 29.06.13.
+//  Copyright (c) 2013 Ida. All rights reserved.
+//
+
+#import "ScreenViewController.h"
+
+@interface ScreenTwentyoneViewController : ScreenViewController
+
+@end
---
+++ Farbwelten/Farbwelten/ScreenTwentyoneViewController.m
@@ -0,0 +1,38 @@
+//
+//  ScreenTwentyoneViewController.m
+//  Farbwelten
+//
+//  Created by Ida Buchwald on 29.06.13.
+//  Copyright (c) 2013 Ida. All rights reserved.
+//
+
+#import "ScreenTwentyoneViewController.h"
+
+@interface ScreenTwentyoneViewController ()
+
+@end
+
+@implementation ScreenTwentyoneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
